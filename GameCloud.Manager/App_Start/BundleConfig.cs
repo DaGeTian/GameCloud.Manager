42d0ca430f77910ef696b12942357fd8336bed45
yan <nbcloudstudio@hotmail.com>
2016-08-08 23:32:51 +0800

    Remove Microsoft.CodeDom.Providers.DotNetCompilerPlatform as its causing werid issues cause site not load

--- GameCloud.Manager/App_Start/BundleConfig.cs
+++ GameCloud.Manager/App_Start/BundleConfig.cs
@@ -13,14 +13,8 @@ namespace GameCloud.Manager
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
                       "~/Scripts/metisMenu.js",
                       "~/Scripts/sb-admin-2.js"));
 
