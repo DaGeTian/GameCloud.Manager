d068a6aa3f268d4311e563bff1745bb213f6b579
nbcloudstudio <nbcloudstudio@hotmail.com>
2016-10-20 02:04:22 +0800

    Add asp;net core identity settings

--- GameCloud.Manager.App/Startup.cs
+++ GameCloud.Manager.App/Startup.cs
@@ -47,6 +47,30 @@ namespace GameCloud.Manager.App
 
             // Add functionality to inject IOptions<T>
             services.AddOptions();
+
+            // Configure asp.net settings
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = false;
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+
+                // Cookie settings
+                options.Cookies.ApplicationCookie.ExpireTimeSpan = TimeSpan.FromDays(150);
+                options.Cookies.ApplicationCookie.LoginPath = "/Account/LogIn";
+                options.Cookies.ApplicationCookie.LogoutPath = "/Account/LogOff";
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
@@ -70,6 +94,8 @@ namespace GameCloud.Manager.App
             }
 
             app.UseStaticFiles();
+
+            app.UseIdentity();
 
             app.UseMvc(routes =>
             {
