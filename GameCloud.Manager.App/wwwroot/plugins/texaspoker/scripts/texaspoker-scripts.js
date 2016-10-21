e82be86809e00ff90279b6fbc9210b1f1394504e
shuai.chen <shuaichen_ax@163.com>
2016-10-21 20:16:08 +0800

    update texaspoker plugins

---
+++ GameCloud.Manager.App/wwwroot/plugins/texaspoker/scripts/texaspoker-scripts.js
@@ -0,0 +1,26 @@
+﻿(function ($enums) {
+
+    var canShowRanking = [{
+        name: 'true',
+        displayName: '显示',
+        value: true
+    }, {
+        name: 'false',
+        displayName: '不显示',
+        value: false
+    }];
+
+    $enums.items.push({ name: 'canShowRanking', items: canShowRanking });
+
+    var canShowSendChips = [{
+        name: 'true',
+        displayName: '可赠送',
+        value: true
+    }, {
+        name: 'false',
+        displayName: '不可赠送',
+        value: false
+    }];
+
+    $enums.items.push({ name: 'canShowSendChips', items: canShowSendChips });
+})($enums || ($enums = {}));
