4e5a756203dac86b939a69ba13e542f0d4bc0171
lion.lee <lion@cragon.cn>
2016-10-24 21:08:44 +0800

    UseUrls添加配置项

--- GameCloud.Manager/GameCloud.Manager.App/Program.cs
+++ GameCloud.Manager/GameCloud.Manager.App/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace GameCloud.Manager.App
 {
@@ -13,9 +17,15 @@ namespace GameCloud.Manager.App
         {
             try
             {
+                var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .Build();
+
                 var host = new WebHostBuilder()
                         .UseKestrel()
                         .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseConfiguration(config)
                         .UseIISIntegration()
                         .UseStartup<Startup>()
                         .Build();
